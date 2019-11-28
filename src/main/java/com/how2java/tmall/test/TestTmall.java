fc6a4ea9b17f3741398122d6a730419bef495b0f
JXY3511088jxy <765101769@qq.com>
2019-11-28 17:22:06 +0800

    tmall_ssm20191128

--- src/main/java/com/how2java/tmall/test/TestTmall.java
+++ src/main/java/com/how2java/tmall/test/TestTmall.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+//测试类
 public class TestTmall {
 
     public static void main(String args[]){
